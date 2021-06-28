4f53d3f4cf2d7299c1f87fb4db9dd334f29441f5
Everton Andrade <everton@andrade.mat.br>
2021-06-28 18:14:42 -0400

    feat: removido configurações e deixado todos os elementos como globais

--- src/install.js
+++ src/install.js
@@ -1,11 +1,7 @@
 import components from "./components/index.js";
-import config from "./config.js";
 
-const install = (Vue, preset) => {
-  (c => (c = Object.assign(c, preset)))(config);
-
-  if (config.global)
-    for (let name in components) Vue.component(name, components[name]);
+const install = Vue => {
+  for (let name in components) Vue.component(name, components[name]);
 };
 
 export default install;
