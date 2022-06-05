7dc75538d0d020b1dacb97a42f9c8c51be6efe8a
Everton Andrade <everton@andrade.mat.br>
2022-06-05 13:14:11 -0400

    chore

--- src/composables/useGridStyle.test.ts
+++
@@ -1,8 +0,0 @@
-import { expect, test } from "vitest";
-import { useGridStyle } from "./useGridStyle";
-
-test("useGridStyle", () => {
-  const { style } = useGridStyle({ color: "white", size: 15 });
-
-  expect(style.value).toEqual({ "--grid-color": "white", "--grid-size": 15 });
-});
